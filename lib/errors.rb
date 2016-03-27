ce449edd42db7fcbc6f10c82cb26965e81bd934f
Suzanne Kozina <kjartan.kozina13@gmail.com>
2016-03-27 02:33:56 +0000

    add Gemfile.lock

--- lib/errors.rb
+++ lib/errors.rb
@@ -1,3 +1,8 @@
 module UdaciListErrors
-  # Error classes go here
+  class InvalidItemTypeError < StandardError 
+  end
+  class IndexExceedsListSizeError < StandardError
+  end
+  class InvalidPriorityValueError < StandardError
+  end
 end
