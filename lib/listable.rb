fec443d7755c4cf14cdaaf83c39470d16dc7beda
Suzanne Kozina <kjartan.kozina13@gmail.com>
2016-03-29 21:45:21 +0000

    some formatting

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,23 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+
+  def format_priority(priority)
+    value = " ⇧".colorize(:red) if priority == "high"
+    value = " ⇨".colorize(:yellow) if priority == "medium"
+    value = " ⇩".colorize(:green) if priority == "low"
+    value = "" if !priority
+    return value
+  end
+
+  def format_date(date1, date2 = nil)
+    if date2 == nil
+      date1 ? date1.strftime("%D") : "No due date"
+    else
+      dates = date1.strftime("%D") if date1
+      dates << " -- " + date2.strftime("%D") if date2
+      dates = "N/A" if !dates
+      return dates
+    end
+  end
 end
--- lib/todo.rb
+++ lib/todo.rb
@@ -3,23 +3,19 @@ class TodoItem
   attr_reader :description, :due, :priority
 
   def initialize(description, options={})
+    # set_type('todo')
     @description = description
-    @due = options[:due] ? Date.parse(options[:due]) : options[:due]
+    @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
-  def format_date
-    @due ? @due.strftime("%D") : "No due date"
-  end
-  def format_priority
-    value = " ⇧" if @priority == "high"
-    value = " ⇨" if @priority == "medium"
-    value = " ⇩" if @priority == "low"
-    value = "" if !@priority
-    return value
-  end
+
   def details
     format_description(@description) + "due: " +
-    format_date +
-    format_priority
+    format_date(@due) +
+    format_priority(@priority)
+  end
+
+  def change_due_date(due_date)
+    @due = Chronic.parse(due_date)
   end
 end
