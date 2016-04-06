8bd2a6764142c59d0ff32f92fe575502251dc2a9
suzannekozina <kjartan.kozina13@gmail.com>
2016-04-06 10:28:02 -0500

    Final touches

--- lib/listable.rb
+++ lib/listable.rb
@@ -17,7 +17,6 @@ module Listable
     elsif options[:start_date] || options[:end_date]
       dates = Chronic.parse(options[:start_date]).strftime("%D").to_s if options[:start_date]
       dates << " -- " + Chronic.parse(options[:end_date]).strftime("%D").to_s if options[:end_date]
-      dates = "N/A" if !dates
       return dates
     else
       return "No due date"
