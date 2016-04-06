22370a341e3d830f0dcb6f1c19397787bd0587f3
suzannekozina <kjartan.kozina13@gmail.com>
2016-04-06 10:25:13 -0500

    Code Clean up

--- lib/listable.rb
+++ lib/listable.rb
@@ -3,9 +3,17 @@ module Listable
     "#{description}".ljust(30)
   end
 
+  def format_priority
+    value = " ⇧".colorize(:red) if priority == "high"
+    value = " ⇨".colorize(:yellow) if priority == "medium"
+    value = " ⇩".colorize(:green) if priority == "low"
+    value = "" if !priority
+    return value
+  end
+
   def format_date(options={})
-    if due = options[:due] != nil
-    return Chronic.parse(options[:due]).strftime("%D").to_s
+    if options[:due] != nil
+      return Chronic.parse(options[:due]).strftime("%D").to_s
     elsif options[:start_date] || options[:end_date]
       dates = Chronic.parse(options[:start_date]).strftime("%D").to_s if options[:start_date]
       dates << " -- " + Chronic.parse(options[:end_date]).strftime("%D").to_s if options[:end_date]
@@ -16,12 +24,7 @@ module Listable
     end
   end
 
-
-  def format_priority(priority)
-    value = " ⇧".colorize(:red) if priority == "high"
-    value = " ⇨".colorize(:yellow) if priority == "medium"
-    value = " ⇩".colorize(:green) if priority == "low"
-    value = "" if !priority
-    return value
+  def format_name
+    @site_name ? @site_name : ""
   end
 end
