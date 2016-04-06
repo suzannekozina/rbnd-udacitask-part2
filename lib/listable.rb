4a80a1e707fb150cc31bb9f2a00bbfb34e768a60
suzannekozina <kjartan.kozina13@gmail.com>
2016-04-05 21:36:18 -0500

    fixed indentation

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,20 +1,20 @@
 module Listable
   def format_description(description)
-		"#{description}".ljust(30)
+    "#{description}".ljust(30)
   end
 
-	def format_date(options={})
-		if due = options[:due] != nil
-			return Chronic.parse(options[:due]).strftime("%D").to_s
-		elseif options[:start_date] || options[:end_date]
-			dates = Chronic.parse(options[:start_date]).strftime("%D").to_s if options[:start_date]
+  def format_date(options={})
+    if due = options[:due] != nil
+    return Chronic.parse(options[:due]).strftime("%D").to_s
+    elsif options[:start_date] || options[:end_date]
+      dates = Chronic.parse(options[:start_date]).strftime("%D").to_s if options[:start_date]
       dates << " -- " + Chronic.parse(options[:end_date]).strftime("%D").to_s if options[:end_date]
       dates = "N/A" if !dates
       return dates
     else
       return "No due date"
-   	end
- 	end
+    end
+  end
 
 
   def format_priority(priority)
